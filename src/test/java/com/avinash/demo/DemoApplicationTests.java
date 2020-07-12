d166481a946671b9377937bc04a2e640645cfa14
m3gastar <47685240+m3gastar@users.noreply.github.com>
2020-07-12 15:00:40 +0530

    Initial commit

---
+++ src/test/java/com/avinash/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
